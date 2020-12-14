203673c4732d4b4127a500f542884a13ca40789d
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-15 00:07:28 +0600

    Client App class

--- Inventory Rest API/Inventory Rest API/App_Start/WebApiConfig.cs
+++ Inventory Rest API/Inventory Rest API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Inventory_Rest_API
 {
@@ -22,6 +23,9 @@ namespace Inventory_Rest_API
 
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
             //config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
